b40c7b0c193d031e20be2538e5ee56a60b1f46ac
Corey Hodge <contact@coreyhodge.net>
2020-01-03 21:43:32 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -8,7 +8,7 @@ def state_log(star_date)
 end
 
 def crew
-  yield (["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"])
+  ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
 end
 
 def greet_crew (crew)
