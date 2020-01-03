724b70673357c2bd3c9f57dc93ca0bf5000c3070
Corey Hodge <contact@coreyhodge.net>
2020-01-03 21:37:19 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,10 +1,9 @@
 def snake_it_up(string)
-  newS = string
   if string[0] == "s"
-    10.times do newS.insert(0,"s")
+    10.times do string.insert(0,"s")
     end
   else
-    newS
+    string
   end
-  newS
+  string
 end
