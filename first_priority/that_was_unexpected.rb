4f92b4008340165d39fb150325082f98c956ea1f
Corey Hodge <contact@coreyhodge.net>
2020-01-03 20:57:04 +0000

    Automatically backed up by Learn

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -1,4 +1,4 @@
-require './lib/debugging-practice-debugging-with-pry-lab-denver-web-010620/first_priority/you_called.rb'
+#require './lib/debugging-practice-debugging-with-pry-lab-denver-web-010620/first_priority/you_called.rb'
 #/home/maigah/.atom/.learn-ide/home/generic-macro-1462/debugging-practice-debugging-with-pry-lab-denver-web-010620
 
 class Person
